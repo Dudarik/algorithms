a9d9219403b3246fda7afeb726a61ead5f4a186d
Alex <50113949+Dudarik@users.noreply.github.com>
2020-02-11 20:26:16 +0500

    быстрая сортировка

--- js/part4.js
+++ js/part4.js
@@ -14,14 +14,16 @@ function qsort(arr) {
   }
   let pivot = arr[0],
     less = arr.slice(1).filter((el) => {
-      console.log('Меньше ', pivot, el);
+      //console.log('Меньше ', pivot, el);
 
       return el <= pivot
     }),
     greater = arr.slice(1).filter((el) => {
-      console.log('Больше', pivot, el);
+      //console.log('Больше', pivot, el);
       return el > pivot
     })
+  console.log(pivot, less, greater);
+
 
   return qsort(less).concat([pivot], qsort(greater))
 }
@@ -64,4 +66,75 @@ function toBin(x, arr) {
   return Number(arr.join(''))
 
 }
-console.log(toBin(5, []))
+//console.log(toBin(5, []))
+
+function qs(arr) {
+
+  if (arr.length < 2) return arr
+
+  let pivot = arr[Math.round(arr.length / 2)]
+
+  let less = arr.filter(el => el < pivot)
+
+  let greater = arr.filter(el => el > pivot)
+
+  let equal = arr.filter(el => el == pivot)
+
+  console.log(pivot, less, greater);
+
+  //return [...qs(less), ...equal, ...qs(greater)]
+  return qs(less).concat(equal, qs(greater))
+}
+
+function bubbleSort(arr) {
+  let len = arr.length
+
+  for (let i = len; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+        /*let tmp = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j + 1] = tmp*/
+      }
+    }
+  }
+  return arr
+}
+
+function partition(arr, low, hi){
+  
+  let pivotPosition = Math.floor(Math.random() * arr.length)
+  let pivot = arr[pivotPosition]
+
+  while (hi >= low) {
+
+    while (arr[hi] > pivot) 
+      hi--
+    
+    while (arr[low] < pivot) 
+      low++
+    
+    if (hi >= low)  {
+      [arr[low], arr[hi]] = [arr[hi], arr[low]]
+      hi--
+      low++
+    }
+  }
+  return low
+}
+
+function qs2(arr, low = 0, hi = arr.length - 1){
+  if (low < hi) {
+    let id = partition(arr, low, hi)
+    qs2(arr, low, id - 1)
+    qs2(arr, id, hi)
+  }
+  return arr
+}
+
+let testQs = [6, 8, 9, 23, 3, 4, 23, 7, 5, 3, 1, 45, 64, 25, 3]
+
+console.log(qs2(testQs));
+
+//console.log(qs(testQs), 'test qs');
